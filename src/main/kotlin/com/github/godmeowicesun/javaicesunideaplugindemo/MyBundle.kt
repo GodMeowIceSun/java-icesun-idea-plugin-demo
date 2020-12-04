1fe822620a8b3c7bf0cd1263da33b7a862999ecb
GitHub Action <action@github.com>
2020-12-04 02:38:11 +0000

    Template cleanup

--- src/main/kotlin/com/github/godmeowicesun/javaicesunideaplugindemo/MyBundle.kt
+++ src/main/kotlin/com/github/godmeowicesun/javaicesunideaplugindemo/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.godmeowicesun.javaicesunideaplugindemo
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/godmeowicesun/javaicesunideaplugindemo/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.godmeowicesun.javaicesunideaplugindemo.services
+
+import com.github.godmeowicesun.javaicesunideaplugindemo.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
